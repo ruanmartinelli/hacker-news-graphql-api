f2aeddc711edaaa3a748a2f2d896299ca77d22ce
Ruan Martinelli <martinelliruan@gmail.com>
2017-12-17 12:32:22 -0200

    add item endpoint

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,11 +1,25 @@
 import schema from '../src/schema'
 import { graphql } from 'graphql'
 
-it('should work for hello world query', async () => {
-  const query = `query Q { hello }`
+it('should fetch an item given an id', async () => {
+  const query = `
+    query Q ($id: Int!) {
+      item(id: $id) {
+        id,
+        score,
+        title
+      }
+    }`
 
-  const result = await graphql(schema, query)
+  const root = {}
+  const context = {}
+  const variables = { id: 8863 }
+
+  const result = await graphql(schema, query, root, context, variables)
   const { data } = result
 
-  expect(data.hello).toBe('world')
+  expect(data.item).toBeDefined()
+  expect(data.item).toHaveProperty('id')
+  expect(data.item).toHaveProperty('score')
+  expect(data.item).toHaveProperty('title')
 })
--- src/schema.js
+++ src/schema.js
@@ -1,19 +1,39 @@
+import { get } from 'axios'
 import {
   GraphQLObjectType,
   GraphQLString,
-  GraphQLSchema
-  // GraphQLInt,
-  // GraphQLList,
-  // GraphQLNonNull
+  GraphQLSchema,
+  GraphQLInt,
+  GraphQLList
 } from 'graphql'
 
+const ItemType = new GraphQLObjectType({
+  name: 'Item',
+  description: 'An item from the HN API',
+  fields: () => ({
+    by: { type: GraphQLString },
+    descendants: { type: GraphQLInt },
+    id: { type: GraphQLInt },
+    kids: { type: new GraphQLList(GraphQLInt) },
+    score: { type: GraphQLInt },
+    text: { type: GraphQLString },
+    time: { type: GraphQLInt },
+    title: { type: GraphQLString },
+    type: { type: GraphQLString },
+    url: { type: GraphQLString }
+  })
+})
+
 const RootQuery = new GraphQLObjectType({
-  name: 'RootQueryType',
+  name: 'Query',
   fields: {
-    hello: {
-      type: GraphQLString,
-      resolve () {
-        return 'world'
+    item: {
+      type: ItemType,
+      args: { id: { type: GraphQLInt } },
+      resolve: async (root, args) => {
+        const url = `https://hacker-news.firebaseio.com/v0/item/${args.id}.json`
+        const { data } = await get(url)
+        return data
       }
     }
   }
